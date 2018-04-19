d45ee3f46fc416b48c2789a1e6af2f867cb82f1e
Valtteri Ojanen <valtteri.ojanen@cs.tamk.fi>
2018-04-19 13:18:36 +0300

    Fixed typo from SpawnerHandleInspector

--- New Unity Project/Assets/Code/Editor/Inspectors/SpawnerHandleInspector.cs
+++ New Unity Project/Assets/Code/Editor/Inspectors/SpawnerHandleInspector.cs
@@ -6,57 +6,54 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 
 namespace TankGame.Editor {
+    [UnityEditor.CustomEditor(typeof(CollectibleSpawner))]
     public class SpawnerHandleInspector: UnityEditor.Editor {
+        
+        private CollectibleSpawner targetPM;
+        private Transform handleTransform;
+        private Quaternion handleQuaternion;
 
-        [UnityEditor.CustomEditor(typeof(CollectibleSpawner))]
-        public class PinballManagerInspector: UnityEditor.Editor
+        protected void OnEnable()
         {
-            private CollectibleSpawner targetPM;
-            private Transform handleTransform;
-            private Quaternion handleQuaternion;
+            targetPM = target as CollectibleSpawner;
+            handleTransform = targetPM.transform;
+        }
 
-            protected void OnEnable()
+        /// <summary>
+        /// Display handle for both min and max spawn positions
+        /// </summary>
+        private void OnSceneGUI()
+        {
+            // gives reference for the variable itself so it can be edited directly
+            // without needing to set it afterwards
+            DisplayLaunchPointHandle(ref targetPM.MinSpawnPosition);
+            DisplayLaunchPointHandle(ref targetPM.MaxSpawnPosition);
+        }
+
+        /// <summary>
+        /// Displays handles in the scene and allows them to be moved in editor
+        /// </summary>
+        /// <param name="targetPoint"> position of the handle currently in the world</param>
+        private void DisplayLaunchPointHandle(ref Vector3 targetPoint)
+        {
+            // Sets the handle's rotation based on
+            // if Local or Global mode is active
+            handleQuaternion = Tools.pivotRotation == PivotRotation.Local ?
+                handleTransform.rotation : Quaternion.identity;
+
+            // Handle current position in the world
+            Vector3 point =
+                targetPoint;
+
+            EditorGUI.BeginChangeCheck();
+
+            point = Handles.DoPositionHandle(point, handleQuaternion);
+
+            if(EditorGUI.EndChangeCheck())
             {
-                targetPM = target as CollectibleSpawner;
-                handleTransform = targetPM.transform;
-            }
-
-            /// <summary>
-            /// Display handle for both min and max spawn positions
-            /// </summary>
-            private void OnSceneGUI()
-            {
-                // gives reference for the variable itself so it can be edited directly
-                // without needing to set it afterwards
-                DisplayLaunchPointHandle(ref targetPM.MinSpawnPosition);
-                DisplayLaunchPointHandle(ref targetPM.MaxSpawnPosition);
-            }
-
-            /// <summary>
-            /// Displays handles in the scene and allows them to be moved in editor
-            /// </summary>
-            /// <param name="targetPoint"> position of the handle currently in the world</param>
-            private void DisplayLaunchPointHandle(ref Vector3 targetPoint)
-            {
-                // Sets the handle's rotation based on
-                // if Local or Global mode is active
-                handleQuaternion = Tools.pivotRotation == PivotRotation.Local ?
-                    handleTransform.rotation : Quaternion.identity;
-
-                // Handle current position in the world
-                Vector3 point =
-                    targetPoint;
-
-                EditorGUI.BeginChangeCheck();
-
-                point = Handles.DoPositionHandle(point, handleQuaternion);
-
-                if(EditorGUI.EndChangeCheck())
-                {
-                    Undo.RecordObject(targetPM, "MoveHandle");
-                    targetPoint =
-                        new Vector3(point.x,0.5f,point.z);
-                }
+                Undo.RecordObject(targetPM, "MoveHandle");
+                targetPoint =
+                    new Vector3(point.x,0.5f,point.z);
             }
         }
     }
